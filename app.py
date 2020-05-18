3762d7118b148de03536b3119f6a56668427767c
isdance <isdance2004@hotmail.com>
2020-05-19 08:24:21 +1000

    fix import

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+# from models import Cars
 import os
-
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Cars
+class Cars(db.Model):
+    __tablename__ = 'cars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String())
+    model = db.Column(db.String())
+
+    def __init__(self, type, model):
+        self.type = type
+        self.model = model
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
 
 
 @app.route('/')
